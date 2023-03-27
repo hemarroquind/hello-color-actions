45445b196eae10980e282da14c94c305f6e5becc
Hmarroquind <hmarroquind@masnomina.com.mx>
2023-03-27 09:48:31 -0600

    Bugs fixed

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,13 @@
-const hello = require("./hello");
-describe("My hello", () => {
-  test("works", () => {
-    expect(hello()).toEqual("Hola mundo desde Mexico");
+// const hi = require("./hello");
+// describe("My hello", () => {
+//   test("works", () => {
+//     expect(hi.hello.toEqual("Hola mundo desde Mexico"));
+//   });
+// });
+const { hello } = require("./hello");
+
+describe("hello", () => {
+  test("returns Hola mundo desde Mexico", () => {
+    expect(hello == "Hola mundo desde Mexico");
   });
 });
